012c8468c29a31193a67a9acdb9b16edfda5688b
Pradeep Singh <psrana344@gmail.com>
2026-09-07 17:41:53 +0530

    change local to cloud

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+import streamlit as st
+
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 
@@ -7,7 +9,15 @@ load_dotenv()
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 AVIATION_STACK_API_KEY = os.getenv("AVIATION_STACK_API_KEY")
 OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
-DATABASE_URL = os.getenv("DATABASE_URL")
+
+# DATABASE_URL
+if "DATABASE_URL" in st.secrets:
+    DATABASE_URL = st.secrets["DATABASE_URL"]
+else:
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 def get_llm():
-    return ChatGroq(model=os.getenv("GROQ_MODEL", "openai/gpt-oss-120b"))
+    return ChatGroq(
+        model=os.getenv("GROQ_MODEL", "openai/gpt-oss-120b")
+    )
